b9f0d0f3850b388369d495758697eadcbd0ad0df
Ambrish goswami <156570582+ambrishgoswami@users.noreply.github.com>
2025-07-09 21:27:44 +0530

    First commit message

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_APP_BASE_URL || '/',
+  base: '/', // or '/your-subpath/' if not at root
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
