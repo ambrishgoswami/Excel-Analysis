f9170d72909fd5c89ec091962d1d5d8b62f9f699
Ambrish goswami <156570582+ambrishgoswami@users.noreply.github.com>
2025-07-10 09:42:25 +0530

    Update middleware.js

--- server/middleware.js
+++ server/middleware.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ["https://excel-analysis.netlify.app", "http://localhost:5173"],
+ origin: process.env.CLIENT_URL,
   credentials: true
 }));
 
